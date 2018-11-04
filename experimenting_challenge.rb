b2d1d2d913664ad65782d37aa5513ab42570e40a
Erica Lacy <ericalacy7@gmail.com>
2018-11-04 20:07:32 +0000

    automatically backed up by learn

---
+++ experimenting_challenge.rb
@@ -0,0 +1,17 @@
+
+
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator
+  speakers.each do |badges|
+    puts badge_maker(badges)
+end
+batch_badge_creator
+
+def assign_rooms
+  
+end
+
+def printer
+  
+end
