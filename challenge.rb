783ff8bad1d2512303b206d17504876b9e96415a
Erica Lacy <ericalacy7@gmail.com>
2018-11-04 20:02:08 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -2,7 +2,14 @@ def badge_maker(name)
   puts "Hello, my name is ${name}"
 end
 
+
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator
   
 end
+
+
+def assign_rooms
+  
+end
+
