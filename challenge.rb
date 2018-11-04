e6819caeffafa6142124ff8074e7a738101895a9
Erica Lacy <ericalacy7@gmail.com>
2018-11-04 20:07:38 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -3,19 +3,3 @@ def badge_maker(name)
 end
 
 badge_maker(Erica)
-
-
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-def batch_badge_creator
-  speakers.each do |badges|
-    puts badge_maker(badges)
-end
-batch_badge_creator
-
-def assign_rooms
-  
-end
-
-def printer
-  
-end
